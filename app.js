575a5da3224ada3a361fb5fec15d6354ce7aa2c7
jacobrees <jacob.rees97@yahoo.com>
2021-03-11 12:35:03 +0000

    Fix all javascript linting errors with eslint

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const slides = document.querySelectorAll('.slide')
-const nextBtn = document.querySelector('.nextBtn')
-const prevBtn = document.querySelector('.prevBtn')
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
 
-slides.forEach(function(slide, index){
-    slide.style.left = `${index * 100}%`
-})
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
 
-let counter = 0
+let counter = 0;
 
-function carousel(){
-    if(counter === slides.length){
-        counter = 0
-    }else if(counter < 0){
-        counter = slides.length -1
-    }
-    slides.forEach(function(slide){
-        slide.style.transform = `translateX(-${counter * 100}%)`
-    })
+function carousel() {
+  if (counter === slides.length) {
+    counter = 0;
+  } else if (counter < 0) {
+    counter = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 }
 
-nextBtn.addEventListener('click', function(){
-    counter += 1
-    carousel()
-})
+nextBtn.addEventListener('click', () => {
+  counter += 1;
+  carousel();
+});
 
-prevBtn.addEventListener('click', function(){
-    counter -= 1
-    carousel()
-})
+prevBtn.addEventListener('click', () => {
+  counter -= 1;
+  carousel();
+});
